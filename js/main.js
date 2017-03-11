b47108cf356ea7d0243776fbe38755da28dceb39
Nick Taft <taft82@gmail.com>
2017-03-11 14:17:29 -0500

    using local data sources

--- js/main.js
+++ js/main.js
@@ -1,104 +1,113 @@
 // // code borrowed from http://bl.ocks.org/dwtkns/4973620
 
 d3.select(window)
-    .on("mousemove", mousemove)
-    .on("mouseup", mouseup);
+  .on('mousemove', mousemove)
+  .on('mouseup', mouseup);
 
-var width = 960,
-    height = 500;
+let width = 960;
+let height = 960;
 
-var proj = d3.geo.orthographic()
-    .translate([width / 2, height / 2])
-    .clipAngle(90)
-    .scale(220);
+let proj = d3.geo.orthographic()
+  .translate([width / 2, height / 2])
+  .clipAngle(90)
+  .scale(220 * 2);
 
-var sky = d3.geo.orthographic()
-    .translate([width / 2, height / 2])
-    .clipAngle(90)
-    .scale(280);
+let sky = d3.geo.orthographic()
+  .translate([width / 2, height / 2])
+  .clipAngle(90)
+  .scale(280 * 2);
 
-var path = d3.geo.path().projection(proj).pointRadius(2);
+let path = d3.geo.path().projection(proj).pointRadius(2);
 
-var swoosh = d3.svg.line()
-      .x(function(d) { return d[0] })
-      .y(function(d) { return d[1] })
-      .interpolate("cardinal")
-      .tension(0);
+let swoosh = d3.svg.line()
+  .x(d => d[0])
+  .y(d => d[1])
+  .interpolate('cardinal')
+  .tension(0);
 
-var links = [],
+let links = [],
     arcLines = [];
 
-var svg = d3.select("body").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .on("mousedown", mousedown);
+let svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .on('mousedown', mousedown);
 
 queue()
-    .defer(d3.json, "https://d3js.org/world-110m.v1.json")
-    .defer(d3.json, "http://www.nicktaft.com/vilna/vt-geo-v2.json")
-    .await(ready);
+  .defer(d3.json, '/map_data')
+  .defer(d3.json, '/vt_data')
+  .await(ready);
 
 function ready(error, world, places) {
-  var ocean_fill = svg.append("defs").append("radialGradient")
-        .attr("id", "ocean_fill")
-        .attr("cx", "75%")
-        .attr("cy", "25%");
-      ocean_fill.append("stop").attr("offset", "5%").attr("stop-color", "rgb(200, 200, 240)");
-      ocean_fill.append("stop").attr("offset", "100%").attr("stop-color", "rgb(150, 150, 170)");
+  let ocean_fill = svg.append('defs')
+    .append('radialGradient')
+    .attr('id', 'ocean_fill')
+    .attr('cx', '75%')
+    .attr('cy', '25%');
+  ocean_fill.append('stop')
+    .attr('offset', '5%')
+    .attr('stop-color', 'rgb(200, 200, 240)');
+  ocean_fill.append('stop')
+    .attr('offset', '100%')
+    .attr('stop-color', 'rgb(150, 150, 170)');
 
-  var globe_highlight = svg.append("defs").append("radialGradient")
-        .attr("id", "globe_highlight")
-        .attr("cx", "75%")
-        .attr("cy", "25%");
-      globe_highlight.append("stop")
-        .attr("offset", "5%").attr("stop-color", "rgb(200, 240, 200)")
-        .attr("stop-opacity","0.6");
-      globe_highlight.append("stop")
-        .attr("offset", "100%").attr("stop-color", "rgb(100, 120, 100)")
-        .attr("stop-opacity","0.2");
+  let globe_highlight = svg.append('defs')
+    .append('radialGradient')
+    .attr('id', 'globe_highlight')
+    .attr('cx', '75%')
+    .attr('cy', '25%');
+  globe_highlight.append('stop')
+    .attr('offset', '5%')
+    .attr('stop-color', 'rgb(200, 240, 200)')
+    .attr('stop-opacity','0.6');
+  globe_highlight.append('stop')
+    .attr('offset', '100%')
+    .attr('stop-color', 'rgb(100, 120, 100)')
+    .attr('stop-opacity','0.2');
 
-  var globe_shading = svg.append("defs").append("radialGradient")
-        .attr("id", "globe_shading")
-        .attr("cx", "55%")
-        .attr("cy", "45%");
-      globe_shading.append("stop")
-        .attr("offset","30%").attr("stop-color", "#fff")
-        .attr("stop-opacity","0")
-      globe_shading.append("stop")
-        .attr("offset","100%").attr("stop-color", "#505962")
-        .attr("stop-opacity","0.3")
+  let globe_shading = svg.append('defs')
+    .append('radialGradient')
+    .attr('id', 'globe_shading')
+    .attr('cx', '55%')
+    .attr('cy', '45%');
+  globe_shading.append('stop')
+    .attr('offset','30%').attr('stop-color', '#fff')
+    .attr('stop-opacity','0')
+  globe_shading.append('stop')
+    .attr('offset','100%').attr('stop-color', '#505962')
+    .attr('stop-opacity','0.3')
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class", "noclicks")
-    .style("fill", "url(#ocean_fill)");
+  svg.append('circle')
+    .attr('cx', width / 2).attr('cy', height / 2)
+    .attr('r', proj.scale())
+    .attr('class', 'noclicks')
+    .style('fill', 'url(#ocean_fill)');
 
-  svg.append("path")
+  svg.append('path')
     .datum(topojson.feature(world, world.objects.land))
-    .attr("class", "land noclicks")
-    .attr("d", path);
+    .attr('class', 'land noclicks')
+    .attr('d', path);
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class","noclicks")
-    .style("fill", "url(#globe_highlight)");
+  svg.append('circle')
+    .attr('cx', width / 2).attr('cy', height / 2)
+    .attr('r', proj.scale())
+    .attr('class','noclicks')
+    .style('fill', 'url(#globe_highlight)');
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class","noclicks")
-    .style("fill", "url(#globe_shading)");
+  svg.append('circle')
+    .attr('cx', width / 2).attr('cy', height / 2)
+    .attr('r', proj.scale())
+    .attr('class','noclicks')
+    .style('fill', 'url(#globe_shading)');
 
-  svg.append("g").attr("class","points")
-      .selectAll("text").data(places.features)
-    .enter().append("path")
-      .attr("class", "point")
-      .attr("d", path);
+  svg.append('g').attr('class','points')
+      .selectAll('text').data(places.features)
+    .enter().append('path')
+      .attr('class', 'point')
+      .attr('d', path);
 
   // spawn links between cities as source/target coord pairs
-  for (let i = 1; i < places.features.length; i++) {
+  for (let i = 1; i < places.features.length - 1; i++) {
     links.push({
       source: places.features[i-1].geometry.coordinates,
       target: places.features[i].geometry.coordinates
@@ -107,32 +116,32 @@ function ready(error, world, places) {
 
   // build geoJSON features from links array
   links.forEach(function(e,i,a) {
-    var feature =   { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [e.source,e.target] }}
+    let feature =   { 'type': 'Feature', 'geometry': { 'type': 'LineString', 'coordinates': [e.source,e.target] }}
     arcLines.push(feature)
   })
 
-  svg.append("g").attr("class","arcs")
-    .selectAll("path").data(arcLines)
-    .enter().append("path")
-      .attr("class","arc")
-      .attr("d",path)
+  svg.append('g').attr('class','arcs')
+    .selectAll('path').data(arcLines)
+    .enter().append('path')
+      .attr('class','arc')
+      .attr('d',path)
 
-  svg.append("g").attr("class","flyers")
-    .selectAll("path").data(links)
-    .enter().append("path")
-    .attr("class","flyer")
-    .attr("d", function(d) { return swoosh(flying_arc(d)) })
+  svg.append('g').attr('class','flyers')
+    .selectAll('path').data(links)
+    .enter().append('path')
+    .attr('class','flyer')
+    .attr('d', function(d) { return swoosh(flying_arc(d)) })
 
   refresh();
 }
 
 function flying_arc(pts) {
-  var source = pts.source,
+  let source = pts.source,
       target = pts.target;
 
 // bends the arc trajectory in relation to the starting point
-  var mid = location_along_arc(source, target, 0.7);
-  var result = [ proj(source),
+  let mid = location_along_arc(source, target, 0.7);
+  let result = [ proj(source),
                  sky(mid),
                  proj(target) ]
   return result;
@@ -141,24 +150,24 @@ function flying_arc(pts) {
 
 
 function refresh() {
-  svg.selectAll(".land").attr("d", path);
-  svg.selectAll(".point").attr("d", path);
+  svg.selectAll('.land').attr('d', path);
+  svg.selectAll('.point').attr('d', path);
 
-  svg.selectAll(".arc").attr("d", path)
-    .attr("opacity", function(d) {
-        return fade_at_edge(d)
+  svg.selectAll('.arc').attr('d', path)
+    .attr('opacity', function(d) {
+      return fade_at_edge(d)
     })
 
-  svg.selectAll(".flyer")
-    .attr("d", function(d) { return swoosh(flying_arc(d)) })
-    .attr("opacity", function(d) {
+  svg.selectAll('.flyer')
+    .attr('d', function(d) { return swoosh(flying_arc(d)) })
+    .attr('opacity', function(d) {
       return fade_at_edge(d)
     })
 
 }
 
 function fade_at_edge(d) {
-  var centerPos = proj.invert([width/2,height/2]),
+  let centerPos = proj.invert([width/2,height/2]),
       arc = d3.geo.greatArc(),
       start, end;
   // function is called on 2 different data structures..
@@ -171,22 +180,22 @@ function fade_at_edge(d) {
     end = d.geometry.coordinates[1];
   }
 
-  var start_dist = 1.57 - arc.distance({source: start, target: centerPos}),
+  let start_dist = 1.57 - arc.distance({source: start, target: centerPos}),
       end_dist = 1.57 - arc.distance({source: end, target: centerPos});
 
-  var fade = d3.scale.linear().domain([-.1,0]).range([0,.1])
-  var dist = start_dist < end_dist ? start_dist : end_dist;
+  let fade = d3.scale.linear().domain([-.1,0]).range([0,.1])
+  let dist = start_dist < end_dist ? start_dist : end_dist;
 
   return fade(dist)
 }
 
 function location_along_arc(start, end, loc) {
-  var interpolator = d3.geo.interpolate(start,end);
+  let interpolator = d3.geo.interpolate(start,end);
   return interpolator(loc)
 }
 
 // modified from http://bl.ocks.org/1392560
-var m0, o0;
+let m0, o0;
 function mousedown() {
   m0 = [d3.event.pageX, d3.event.pageY];
   o0 = proj.rotate();
@@ -194,7 +203,7 @@ function mousedown() {
 }
 function mousemove() {
   if (m0) {
-    var m1 = [d3.event.pageX, d3.event.pageY]
+    let m1 = [d3.event.pageX, d3.event.pageY]
       , o1 = [o0[0] + (m1[0] - m0[0]) / 6, o0[1] + (m0[1] - m1[1]) / 6];
     o1[1] = o1[1] > 30  ? 30  :
             o1[1] < -30 ? -30 :
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 
+const vtData = require('./data/vt-geo-v2.json');
+const mapData = require('./data/world-110m.v1.json');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,7 +12,15 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-  res.sendFile('/Users/ntaft/code/personal/vilnatroupe/vilnatroupe-globe-d3/public/index.html');
+  res.sendFile('index.html');
 });
+
+app.get('/vt_data', (req, res) => {
+  res.json(vtData);
+});
+
+app.get('/map_data', (req, res) => {
+  res.json(mapData);
+})
 
 app.listen(port, () => console.log('listening on port', port))
